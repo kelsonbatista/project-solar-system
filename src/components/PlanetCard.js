792e32845352138b5e7fc14b7543a56035137713
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-12-17 15:59:00 -0300

    Requisito 6 css updated

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -5,8 +5,14 @@ class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
-        <div><img src={ planetImage } alt={ `Planeta ${planetName}` } /></div>
+      <div className={ `planet-card ${planetName}Div` } data-testid="planet-card">
+        <div className="planet">
+          <img
+            className={ planetName }
+            src={ planetImage }
+            alt={ `Planeta ${planetName}` }
+          />
+        </div>
         <div data-testid="planet-name">{planetName}</div>
       </div>
     );
